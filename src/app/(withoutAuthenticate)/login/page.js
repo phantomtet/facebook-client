6488338676e9f982ae247f1b490a0b6876b9da75
Vu Nguyen <phantomtet16698@gmail.com>
2024-02-17 20:34:25 +0700

    .

--- src/app/(withoutAuthenticate)/login/page.js
+++ src/app/(withoutAuthenticate)/login/page.js
@@ -1,15 +1,17 @@
-import LoginForm from './components/loginForm'
-import './style.css'
+import LoginForm from "./components/loginForm";
+import "./style.css";
 
 export default function LogIn() {
-    return (
-        <div className='login-container'>
-            <title>Facebook - log in or sign up</title>
-            <div className='description-container'>
-                <img className='img' src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg' />
-                <div className='description'>Facebook helps you connect and share with the people in your life.</div>
-            </div>
-            <LoginForm />
+  return (
+    <div className="login-container">
+      <title>Facebook - log in or sign up</title>
+      <div className="description-container">
+        <img className="img" src="facebook-logo.svg" />
+        <div className="description">
+          Facebook helps you connect and share with the people in your life.
         </div>
-    )
+      </div>
+      <LoginForm />
+    </div>
+  );
 }
